3037fc84dcd803533a1cce446c5bca74ac9d0a48
Josh <jromer94@gmail.com>
2015-01-11 09:56:04 -0500

    added addDrinkVolume();

--- kegapi.js
+++ kegapi.js
@@ -4,13 +4,21 @@ var moment = require('moment');
 function getDrinks() {
   request('http://104.131.47.86:8000/api/drinks', function (error, response, body) {
     var json = JSON.parse(body);
-    checkRecentDrinks(json.objects);
+    addDrinkVolumes(json.objects);
   });
 }
 
 function checkRecentDrinks(drinks) {
   var currentTime = moment();
-  for(var i = 0; i < drinks.length; i++){
+  for(var i = 0; i < drinks.length; i++) {
     console.log(currentTime.diff(drinks[i].time, 'minutes'));
   } 
+}
+
+function addDrinkVolumes(drinks) {
+  var volume = 0;
+  for(var i = 0; i < drinks.length; i++) {
+    volume = volume + drinks[i].volume_ml;
+  }
+  console.log(volume);
 }
