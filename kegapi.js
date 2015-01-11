72ba3be95915040c1051e27cc5c1e9917818af23
Josh <jromer94@gmail.com>
2015-01-11 10:13:00 -0500

    added callbacks to all functions

--- kegapi.js
+++ kegapi.js
@@ -1,24 +1,31 @@
 var request = require('request');
 var moment = require('moment');
 
-function getDrinks() {
+function getDrinks(callback) {
   request('http://104.131.47.86:8000/api/drinks', function (error, response, body) {
     var json = JSON.parse(body);
-    addDrinkVolumes(json.objects);
+    callback = (typeof callback === 'function') ? callback : function() {};
+    callback(json.objects);
   });
 }
 
-function checkRecentDrinks(drinks) {
+function checkRecentDrinks(drinks, callback) {
   var currentTime = moment();
   for(var i = 0; i < drinks.length; i++) {
     console.log(currentTime.diff(drinks[i].time, 'minutes'));
   } 
+  callback = (typeof callback === 'function') ? callback : function() {};
+  callback();
 }
 
-function addDrinkVolumes(drinks) {
+function addDrinkVolumes(drinks, callback) {
   var volume = 0;
   for(var i = 0; i < drinks.length; i++) {
     volume = volume + drinks[i].volume_ml;
   }
   console.log(volume);
+  callback = (typeof callback === 'function') ? callback : function() {};
+  callback(volume);
 }
+
+getDrinks(addDrinkVolumes);
