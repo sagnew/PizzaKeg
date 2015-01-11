33cb3f11e2eed0fe91d3bd0235caa08ab17beffe
Sam Agnew <sagnew@eden.rutgers.edu>
2015-01-11 11:24:45 -0500

    Twilio stuff

--- server.js
+++ server.js
@@ -3,26 +3,22 @@ var twilio = require('twilio'),
 
 var app = express();
 
-app.get('/', function(req, res) {
-    console.log('fuckkkk');
-    res.send('lolol');
-});
-
-app.listen('7890', function() {
-    console.log('Visit http://localhost:7890/ in your browser to see your TwiML document!');
-});
-
 var textResponse = function(request, response) {
     console.log('got here');
-    if (twilio.validateExpressRequest(request, config.twilio.key, {
-            url: config.twilio.smsWebhook
-        }) || config.disableTwilioSigCheck) {
+    if (twilio.validateExpressRequest(request, '08b1eaf92fd66749470c93088253c7b4', { url: 'http://samshreds.com:7890/text' })) {
         response.header('Content-Type', 'text/xml');
         var body = request.param('Body').trim();
         var from = request.param('From');
+        console.log(body);
         response.send('<Response><Sms>LOL.</Sms></Response>');
     } else {
         response.statusCode = 403;
         response.render('forbidden');
     }
 };
+
+app.get('/text', textResponse);
+
+app.listen('7890', function() {
+    console.log('Visit http://localhost:7890/ in your browser to see your TwiML document!');
+});
