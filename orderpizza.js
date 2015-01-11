f7d6727ca1fa02cd7b824f01ceb3c13fd44a2f6d
Tobias Perelstein <tobias.perelstein@gmail.com>
2015-01-11 11:22:59 -0500

    Address issue with trays, tips, cuisines and place order

--- orderpizza.js
+++ orderpizza.js
@@ -5,21 +5,23 @@ var request = require('request');
 var ordrin_api = new  ordrin.APIs('lWTtLN-VlscFum_EuSgqguuhP5WNdwTKWRblNGGAH-Y', ordrin.TEST);
 
 function place_pizza_order(rid,tray,price){
+    var tip = parseFloat(price,10)*.2.toFixed(2);
+    console.log(tip);
     var args = {
         rid: rid.toString(),
-        em: 'sam@slam-minded.org',
+        em: 'detox27@gmail.com',
         tray: tray,
-        //tip: (parseFloat(price,10)*.2).toString(),
-        tip: '1.39',
+        //tip: (parseFloat(price,10)*.2).round(2).toString(),
+        tip: '6.66',
         first_name: 'Slammin',
         last_name: 'Sammy',
         phone: '6666666666',
-        zip: '08901',
-        addr: '31 High Street',
-        city: 'New Brunswick',
-        state: 'NJ',
+        zip: '10036',
+        addr: '1500 Broadway',
+        city: 'New York',
+        state: 'NY',
         card_name: 'Slammin Sammy',
-        card_number: '000000000000000',
+        card_number: '4111111111111111',
         card_cvc: '123',
         card_expiry: '06/2006',
         card_bill_addr: '31 High Street',
@@ -29,13 +31,12 @@ function place_pizza_order(rid,tray,price){
         card_bill_phone: '5555555555',
         delivery_date: 'ASAP'
     };
-    console.log(args);
     ordrin_api.order_guest(args, function(error,data){
         if(error){
             console.log(error);
         }
         else{
-            console.log(data);
+            console.log('Order Successful');
         }
 
     });
@@ -43,17 +44,21 @@ function place_pizza_order(rid,tray,price){
 }
 
 function get_tray_info(rid){
-    console.log(rid);
-    var url = 'http://foodbot.ordr.in:8000/TextSearch?rid='+rid+'&target=pie';
+    var url = 'http://foodbot.ordr.in:8000/TextSearch?rid='+rid+'&target=pizza pie';
     request({
         url: url,
         json: true
     }, function (error, response, body) {
         if (!error && response.statusCode === 200) {
-            tray = body[0].tray;
-            price = body[0].price;
-            place_pizza_order(rid,tray,price);
-            console.log([tray,price]);
+            for(var i=0; i<body.length; i++){
+                if(body[i].tray){
+                    tray = body[i].tray;
+                    price = body[i].price;
+                    place_pizza_order(rid,tray,price);
+                    console.log([tray,price]);
+                    break;
+                }
+            }
         }
         else{
             console.log(error);
@@ -68,19 +73,22 @@ function get_tray_info(rid){
 function order_pizza(){
     var args = {
         datetime: 'ASAP',
-        addr: '31 High Street',
-        city: 'New Brunswick',
-        zip: '08901'
+        addr: '1515 Broadway',
+        city: 'New York',
+        zip: '10036'
     };
     ordrin_api.delivery_list(args,function(error,restaurants){
         relative_rids = [];
         var cuisines = ['Italian','Pizza'];
         for(index in restaurants){
-            //Takes the intersection bewteen the cusines for each restauruant and the var cuisines
+            //Takes the intersection bewteen the cuisines for each restauruant and the var cuisines
             //( ͡° ͜ʖ ͡°)
             //( ͡° ͜ʖ ͡°)
-            if(array(array(restaurants[index].cu).intersect(cuisines))._value.length >=1){
-                relative_rids.push(restaurants[index].id);
+            //console.log(array(restaurants[index].cu);
+            if(restaurants[index].hasOwnProperty('cu')){
+                if(array(array(restaurants[index].cu).intersect(cuisines))._value.length >=1){
+                    relative_rids.push(restaurants[index].id);
+                }
             }
         }
         var rand_rid_index = Object.keys(relative_rids)[Math.floor(Math.random()*relative_rids.length)];
